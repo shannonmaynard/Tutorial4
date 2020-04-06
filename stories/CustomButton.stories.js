63c5d87750d435e78bb32984a7b13f01665cdfdd
Shannon Maynard <shannonmaynard@Shannons-MacBook-Pro-5.local>
2020-04-06 00:13:48 -0700

    neww

--- stories/CustomButton.stories.js
+++ stories/CustomButton.stories.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import CustomButton from '../comps/CustomButton';
+import Header from '../comps/Header';
 
 export default{
     title: "Custom Button",
@@ -9,15 +10,47 @@ export default{
 export const MyCustomButton = () => <CustomButton/>;
 
 export const PageWithCustomButtons = () => <div>
+   <Header 
+   fontSize={15}
+   color="#ABC"
+   onMouseOver={CancelClick}
+   />
+   
+   
     <CustomButton 
     color="#999"
-    text="Cancel"/>
+    text="Cancel"
+    onClick={CancelClick}
+    />
+    
     <CustomButton 
     color="#3F5"
-    text ="OK"/>
+    text ="OK"
+    onClick={OKClick}
+    />
     <CustomButton 
     color="#F3F"
-    text ="Submit"/>
+    text ="Submit"
+    onClick={SubmitClick}
+    />
     <CustomButton 
-    text ="Menu"/>
+    text ="Menu"
+    onClick={MenuClick}
+    />
 </div>
+
+function CancelClick(){
+    alert("Cancel");
+}
+
+function OKClick(){
+    alert("OK");
+}
+
+function SubmitClick(){
+    alert("Submit");
+}
+
+function MenuClick(){
+    alert("Menu");
+}
