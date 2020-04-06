5dfea2d7a2277fc5d13367b15e48de7b86ad7254
Shannon Maynard <shannonmaynard@Shannons-MacBook-Pro-5.local>
2020-04-05 22:51:17 -0700

    new

---
+++ stories/CustomButton.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import CustomButton from '../comps/CustomButton';
+
+export default{
+    title: "Custom Button",
+    component:CustomButton
+};
+
+export const MyCustomButton = () => <CustomButton/>;
