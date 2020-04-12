d80a1d52ebcaffa77f915f380001617ae7f6d989
Shannon Maynard <shannonmaynard@Shannons-MacBook-Pro-5.local>
2020-04-12 14:00:06 -0700

    new

--- pages/ChatPage/index.js
+++ pages/ChatPage/index.js
@@ -5,11 +5,15 @@ import CustomButton from '../../comps/CustomButton'
 import Header from '../../comps/Header';
 import Input from '../../comps/Input';
 
+function ChangeWelcome(){
+    document.querySelector("#welcome").innerText = "Start with sending a message.";
+}
+
 const ChatPage = () => <div>
 <div id="welcome">
     <Header fontSize={32} text="Welcome to my App!"></Header>
 </div>
-<div id="chats">
+<div id="chats" onClick={ChangeWelcome}>
     <Chat />
 </div>
 <div id="controls">
